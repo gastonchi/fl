6a992daea93b3258694d5a2f9825681ba6a2f7ad
gastonchi <109993634+gastonchi@users.noreply.github.com>
2022-08-01 20:22:42 -0300

    Update ejercicio1-a.py

--- ejercicio1-a.py
+++ ejercicio1-a.py
@@ -1,6 +1,7 @@
 
 ### Ejercicio 1 a) #############################################
 # VERSION 1 
+
 a1=[1,2,3,4]
 a2=[-1,0,1,-2]
 a3=[-1,-2,0,3]
@@ -16,7 +17,7 @@ while i < len(a1):
 print (x)
 
 #VERSION 2
-
+import numpy as np
 a1=[1,2,3,4]
 a2=[-1,0,1,-2]
 a3=[-1,-2,0,3]
@@ -25,12 +26,11 @@ x1=[]
 for x in range(0,len(a1)):
   ecuacion = 3*a1[x]-2*a2[x]+4*a3[x]
   x1.append(ecuacion)    
-
-print(x1)
+print(np.array(x1))
 
 
 ### Ejercicio 1 b)
-
+import numpy as np
 A=np.array([[1,-1,-1],[2,0,-2],[3,1,0],[4,-2,3]]) 
 c=np.array([3,-2,4])
 x2=np.dot(A,c)
